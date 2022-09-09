73312e54ae7a1e3007197c97448f5a309c1f2999
jenkarevalo <jenkarevalo@gmail.com>
2022-09-09 18:48:53 -0500

    Cambios backend Relacion paciente-Medico

--- HospiEnCasa.App.Consola/Program.cs
+++ HospiEnCasa.App.Consola/Program.cs
@@ -3,13 +3,13 @@ using System;
 using HospiEnCasa.App.Dominio;
 using HospiEnCasa.App.Persistencia;
 
-public class Program 
+public class Program
 {
-   private static IRepositorioPaciente _repositorioPaciente= new RepositorioPaciente(new HospiEnCasa.App.Persistencia.AppContext());
-   private static IRepositorioMedico _repositorioMedico= new RepositorioMedico(new HospiEnCasa.App.Persistencia.AppContext());
-   private static IRepositorioEnfermera _repositorioEnfermera= new RepositorioEnfermera(new HospiEnCasa.App.Persistencia.AppContext());
-   private static IRepositorioFamiliarDesignado _repositorioFamiliarDesignado= new RepositorioFamiliarDesignado(new HospiEnCasa.App.Persistencia.AppContext());
-   private static void Main (String[] args)
+   private static IRepositorioPaciente _repositorioPaciente = new RepositorioPaciente(new HospiEnCasa.App.Persistencia.AppContext());
+   private static IRepositorioMedico _repositorioMedico = new RepositorioMedico(new HospiEnCasa.App.Persistencia.AppContext());
+   private static IRepositorioEnfermera _repositorioEnfermera = new RepositorioEnfermera(new HospiEnCasa.App.Persistencia.AppContext());
+   private static IRepositorioFamiliarDesignado _repositorioFamiliarDesignado = new RepositorioFamiliarDesignado(new HospiEnCasa.App.Persistencia.AppContext());
+   private static void Main(String[] args)
    {
       Console.WriteLine("Hello, World!");
 
@@ -19,19 +19,42 @@ public class Program
       //AdicionarMedico();
       //BuscarMedico();
       //AdicionarEnfermera();
-      BuscarEnfermera();
+      //BuscarEnfermera();
       //AdicionarFamiliarDesignado();
       //BuscarFamiliarDesignado();
+      EscogerMedico();
+   }
+
+   public static void EscogerMedico()
+   {
+      Medico medico = _repositorioMedico.GetMedico(2);
+      Console.WriteLine("Medico: " + medico.Nombre);
+      medico.ListaPacientes = _repositorioPaciente.GetPacientesXMedico(medico.Id).ToList();
+
+      Paciente paciente = new Paciente();
+      paciente.Nombre = "Jose";
+      paciente.Apellido = "Osorio";
+      paciente.Telefono = "3216874862";
+      paciente.Genero = Genero.masculino;
+      paciente.Direccion = "calle 127 # 12-05";
+      paciente.Ciudad = "Bogota";
+      paciente.FechaNacimiento = new DateTime(1987, 08, 12);
+
+      paciente.Medico = medico;
+      medico.ListaPacientes.Add(paciente);
+
+      _repositorioMedico.UpdateMedico(medico);
+      Console.WriteLine("Accion realizada");
    }
    static void AdicionarPaciente()
    {
-      Console.WriteLine ("Adicionando un Paciente");
+      Console.WriteLine("Adicionando un Paciente");
       Paciente paciente = new Paciente();
       paciente.Nombre = "Ale";
       paciente.Apellido = "Cabas";
       paciente.Telefono = "315282567";
       paciente.Genero = Genero.femenino;
-      paciente.Direccion ="Calle 256No. 13-48";
+      paciente.Direccion = "Calle 256No. 13-48";
       paciente.Ciudad = "Santa Marta";
       paciente.FechaNacimiento = DateTime.Now;
 
@@ -40,7 +63,7 @@ public class Program
    }
    static void BuscarPaciente()
    {
-      var paciente =_repositorioPaciente.GetPaciente(1);
+      var paciente = _repositorioPaciente.GetPaciente(1);
       Console.WriteLine("Nombre: " + paciente.Nombre);
       Console.WriteLine("Apellido: " + paciente.Apellido);
    }
@@ -50,19 +73,19 @@ public class Program
       var lspacientes = _repositorioPaciente.GetAllPacientes();
       foreach (var paciente in lspacientes)
       {
-         Console.WriteLine ("Nombre: " + paciente.Nombre);
-         Console.WriteLine ("Telefono: " + paciente.Telefono);
+         Console.WriteLine("Nombre: " + paciente.Nombre);
+         Console.WriteLine("Telefono: " + paciente.Telefono);
       }
    }
    static void AdicionarMedico()
    {
-      Console.WriteLine ("Adicionando un Medico");
+      Console.WriteLine("Adicionando un Medico");
       Medico medico = new Medico();
       medico.Nombre = "Juan";
       medico.Apellido = "Rivera";
       medico.Telefono = "3171596482";
       medico.Genero = Genero.masculino;
-      medico.Codigo ="15415123";
+      medico.Codigo = "15415123";
       medico.Especialidad = "Ortopedia";
 
       _repositorioMedico.AddMedico(medico);
@@ -70,7 +93,7 @@ public class Program
    }
    static void BuscarMedico()
    {
-      var medico =_repositorioMedico.GetMedico(1);
+      var medico = _repositorioMedico.GetMedico(1);
       Console.WriteLine("Nombre: " + medico.Nombre);
       Console.WriteLine("Apellido: " + medico.Apellido);
    }
@@ -79,19 +102,19 @@ public class Program
       var lsMedicos = _repositorioMedico.GetAllMedicos();
       foreach (var medico in lsMedicos)
       {
-         Console.WriteLine ("Nombre: " + medico.Nombre);
-         Console.WriteLine ("Telefono: " + medico.Telefono);
+         Console.WriteLine("Nombre: " + medico.Nombre);
+         Console.WriteLine("Telefono: " + medico.Telefono);
       }
    }
    static void AdicionarEnfermera()
    {
-      Console.WriteLine ("Adicionando una Enfermera");
+      Console.WriteLine("Adicionando una Enfermera");
       Enfermera enfermera = new Enfermera();
       enfermera.Nombre = "Rosa";
       enfermera.Apellido = "Ortega";
       enfermera.Telefono = "3208714963";
       enfermera.Genero = Genero.femenino;
-      enfermera.TarjetaProfesional ="15415123";
+      enfermera.TarjetaProfesional = "15415123";
       enfermera.HorasLaborales = 545;
 
       _repositorioEnfermera.AddEnfermera(enfermera);
@@ -99,19 +122,19 @@ public class Program
    }
    static void BuscarEnfermera()
    {
-      var enfermera =_repositorioEnfermera.GetEnfermera(1);
+      var enfermera = _repositorioEnfermera.GetEnfermera(1);
       Console.WriteLine(enfermera.Nombre + " " + enfermera.Apellido);
       Console.WriteLine("TarjetaProfesional: " + enfermera.TarjetaProfesional);
    }
    static void AdicionarFamiliarDesignado()
    {
-      Console.WriteLine ("Adicionando un Familiar Designado");
+      Console.WriteLine("Adicionando un Familiar Designado");
       FamiliarDesignado familiarDesignado = new FamiliarDesignado();
       familiarDesignado.Nombre = "Rodrigo";
       familiarDesignado.Apellido = "Alvarez";
       familiarDesignado.Telefono = "3025896715";
       familiarDesignado.Genero = Genero.masculino;
-      familiarDesignado.Parentesco ="Hermano";
+      familiarDesignado.Parentesco = "Hermano";
       familiarDesignado.Correo = "Rodri.al@gmail.com";
       familiarDesignado.PacienteId = 1;
 
@@ -120,7 +143,7 @@ public class Program
    }
    static void BuscarFamiliarDesignado()
    {
-      var familiarDesignado =_repositorioFamiliarDesignado.GetFamiliarDesignado(4);
+      var familiarDesignado = _repositorioFamiliarDesignado.GetFamiliarDesignado(4);
       Console.WriteLine(familiarDesignado.Nombre + " " + familiarDesignado.Apellido);
       Console.WriteLine("Parentesco: " + familiarDesignado.Parentesco);
    }
